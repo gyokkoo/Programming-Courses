ab8a405ae1b0b16be917e052a0ec05f936968275
Nezhdetov <gyokan.nezhdetov@gmail.com>
2015-08-05 00:16:21 +0300

    First topic exercises | all done
    Two easy tasks from the first topic.

---
+++ 1.Fundamental-Level/1.Advanced-C#/01.Arrays-Lists-Stacks-Queues-Exercises/1.BubbleSort/BubbleSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+/*
+Write a program to sort an array of integer numbers and then print them back on the console. 
+The numbers should be entered from the console on a single line, separated by a space. 
+Print the sorted array in the following format: “[element1, element2… elementN]”.
+*/
+class BubbleSort
+{
+    static void Main()
+    {
+        Console.Title = "Problem 1. Sort Array of Numbers Using Bubble Sort";
+
+        Console.WriteLine("Enter numbers on a singe line, separated by a space.");
+        Console.Write("Enter order  ---> ");
+
+        int[] arrNumbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        for (int i = 0; i < arrNumbers.Length; i++)
+        {
+            BubbleSortStep(arrNumbers);
+        }
+
+        Console.Write("Sorted order ---> ");
+        Console.WriteLine(string.Join(" ", arrNumbers));       
+    }
+
+    static int[] BubbleSortStep(int[] arrNumbers)
+    {
+        int temp = arrNumbers[0];
+        for (int i = 1; i <= arrNumbers.Length - 1; i++)
+        {
+            if (arrNumbers[i - 1] > arrNumbers[i])
+            {
+                temp = arrNumbers[i - 1];
+                arrNumbers[i - 1] = arrNumbers[i];
+                arrNumbers[i] = temp;
+            }
+        }
+        return arrNumbers;
+    }
+}
---
+++ 1.Fundamental-Level/1.Advanced-C#/01.Arrays-Lists-Stacks-Queues-Exercises/2.JoinLists/JoinLists.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+/*
+Write a program that takes as input two lists of integers and joins them. 
+The result should hold all numbers from the first list, and all numbers from the second list, 
+without repeating numbers, and arranged in increasing order. The input and output lists
+are given as integers, separated by a space, each list at a separate line. Do not use LINQ!
+Use only arrays and lists.  
+ */
+class JoinLists
+{
+    static void Main()
+    {
+        Console.Title = "Problem 2. Join Lists";
+        Console.WriteLine("Enter two lists of integers, seperated by a space.");
+
+        List<int> list = new List<int>();
+
+        Console.Write("First list  ---> ");
+        string firstInput = Console.ReadLine();
+        string[] firstInputStrArr = firstInput.Split(' ');
+        int[] firstNumsArr = new int[firstInputStrArr.Length];
+
+        for (int i = 0; i < firstNumsArr.Length; i++)
+        {
+            firstNumsArr[i] = int.Parse(firstInputStrArr[i]);
+            if (list.IndexOf(firstNumsArr[i]) == -1)
+            {
+                list.Add(firstNumsArr[i]);
+            }
+        }
+
+        Console.Write("Second list ---> ");
+        string secondInput = Console.ReadLine();
+        string[] secondInputStrArr = secondInput.Split(' ');
+        int[] secondNumsArr = new int[secondInputStrArr.Length];
+
+        for (int i = 0; i < secondNumsArr.Length; i++)
+        {
+            secondNumsArr[i] = int.Parse(secondInputStrArr[i]);
+            if (list.IndexOf(secondNumsArr[i]) == -1)
+            {
+                list.Add(secondNumsArr[i]);
+            }
+        }
+
+        list.Sort();
+
+        foreach (int num in list)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+}
