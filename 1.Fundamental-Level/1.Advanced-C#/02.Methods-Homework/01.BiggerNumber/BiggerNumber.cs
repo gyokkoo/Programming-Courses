0f380e6692d6761b6887132f61466f7b1c058fa9
Nezhdetov <gyokan.nezhdetov@gmail.com>
2015-08-08 22:23:13 +0300

    Second topic homework | in progress
    Add first problem

---
+++ 1.Fundamental-Level/1.Advanced-C#/02.Methods-Homework/01.BiggerNumber/BiggerNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+/*
+Write a method GetMax() with two parameters that returns the larger of two integers. 
+Write a program that reads 2 integers from the console and prints the largest of them using the method GetMax().
+ */
+class BiggerNumber
+{
+    static void Main()
+    {
+        Console.Title = "Problem 1.	Bigger Number";
+        Console.WriteLine("Enter two integers, each on a separate line.");
+
+        int firstNumber = int.Parse(Console.ReadLine());
+        int secondNumber = int.Parse(Console.ReadLine());
+
+        int max = GetMax(firstNumber, secondNumber);
+        Console.WriteLine(max);
+    }
+
+    static int GetMax(int firstNumber, int secondNumber)
+    {
+        int maxNumber = 0;
+
+        if (firstNumber >= secondNumber)
+        {
+            maxNumber = firstNumber;
+        }
+        else
+        {
+            maxNumber = secondNumber;
+        }
+
+        return maxNumber;
+    }
+}
