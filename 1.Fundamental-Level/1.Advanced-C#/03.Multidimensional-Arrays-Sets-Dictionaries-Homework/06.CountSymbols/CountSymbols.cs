1bf23049ec901b028ca5d0444d14d12d0029a614
Nezhdetov <gyokan.nezhdetov@gmail.com>
2015-08-12 22:45:40 +0300

    Third topic homework | almost done
    In the half way...

---
+++ 1.Fundamental-Level/1.Advanced-C#/03.Multidimensional-Arrays-Sets-Dictionaries-Homework/06.CountSymbols/CountSymbols.cs
@@ -0,0 +1,12 @@
+﻿using System;
+/*
+Write a program that reads some text from the console and counts the occurrences of each character in it. 
+Print the results in alphabetical (lexicographical) order. Examples:
+ */
+class CountSymbols
+{
+    static void Main()
+    {
+        Console.Title = "Problem 6.	Count Symbols";
+    }
+}
