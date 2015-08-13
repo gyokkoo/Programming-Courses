033138784c64a6285e1fc602f6e0a8aa6cdc1d39
Nezhdetov <gyokan.nezhdetov@gmail.com>
2015-08-13 20:57:50 +0300

    Third topic homework | TODO *tasks
    Almost everything is done :)

--- 1.Fundamental-Level/1.Advanced-C#/03.Multidimensional-Arrays-Sets-Dictionaries-Homework/06.CountSymbols/CountSymbols.cs
+++ 1.Fundamental-Level/1.Advanced-C#/03.Multidimensional-Arrays-Sets-Dictionaries-Homework/06.CountSymbols/CountSymbols.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 /*
 Write a program that reads some text from the console and counts the occurrences of each character in it. 
 Print the results in alphabetical (lexicographical) order. Examples:
@@ -8,5 +10,23 @@ class CountSymbols
     static void Main()
     {
         Console.Title = "Problem 6.	Count Symbols";
+        Console.WriteLine("Enter some text.");
+
+        char[] text = Console.ReadLine().ToCharArray();
+
+        var symbolsDictionary = new SortedDictionary<char, int>();
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if(!symbolsDictionary.ContainsKey(text[i]))
+            {
+                symbolsDictionary.Add(text[i], text.Count(x => x == text[i]));
+            }
+        }
+
+        foreach (var symbol in symbolsDictionary)
+        {
+            Console.WriteLine("{0}: {1} time/s", symbol.Key, symbol.Value);
+        }
     }
 }
