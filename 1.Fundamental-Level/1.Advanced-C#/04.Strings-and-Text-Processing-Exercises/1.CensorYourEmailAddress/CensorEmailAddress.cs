9b3d75b3fda308abc0b4531be6b2871dae0dec44
Nezhdetov <gyokan.nezhdetov@gmail.com>
2015-08-17 19:25:36 +0300

    Fourth Topic Exercises | All done
    add String and Text processing exercises

---
+++ 1.Fundamental-Level/1.Advanced-C#/04.Strings-and-Text-Processing-Exercises/1.CensorYourEmailAddress/CensorEmailAddress.cs
@@ -0,0 +1,31 @@
+﻿using System;
+/*
+Input:
+ pesho.peshev@email.bg
+My name is Pesho Peshev. I am from Sofia, my email is: pesho.peshev@email.bg (not pesho.peshev@email.com). Test: pesho.meshev@email.bg, pesho.peshev@email.bg
+Output:
+ My name is Pesho Peshev. I am from Sofia, my email is: ************@email.bg (not pesho.peshev@email.com). Test: pesho.meshev@email.bg, ************@email.bg
+
+ */
+class CensorEmailAddress
+{
+    static void Main()
+    {
+        Console.Title = "Problem 1. Censor Your Email Address";
+
+        string correctEmail = Console.ReadLine();
+        string text = Console.ReadLine();
+
+        int indexOfDomainStart = correctEmail.IndexOf('@');
+
+        string username = correctEmail.Substring(0, indexOfDomainStart);
+        string domain = correctEmail.Substring(indexOfDomainStart, (correctEmail.Length - username.Length));
+       
+        string replacementString = new string('*', username.Length) + domain;
+
+        text = text.Replace(correctEmail, replacementString );
+
+        Console.WriteLine("\nOutput:");
+        Console.WriteLine(text);
+    }
+}
