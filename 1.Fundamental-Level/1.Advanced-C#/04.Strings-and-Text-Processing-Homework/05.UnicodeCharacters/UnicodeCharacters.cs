69ac3e21af62b0bc1d8494fe72721f0335b672a0
Gyokan <Nezhdetov@users.noreply.github.com>
2015-09-02 22:06:30 +0300

    Update UnicodeCharacters.cs

--- 1.Fundamental-Level/1.Advanced-C#/04.Strings-and-Text-Processing-Homework/05.UnicodeCharacters/UnicodeCharacters.cs
+++ 1.Fundamental-Level/1.Advanced-C#/04.Strings-and-Text-Processing-Homework/05.UnicodeCharacters/UnicodeCharacters.cs
@@ -2,7 +2,7 @@
 /*
 Write a program that converts a string to a sequence of C# Unicode character literals. Examples:
 Input       	Output
-Hi!	            \u0048\u0069\u0021
+Hi!	        \u0048\u0069\u0021
 
  */
 class UnicodeCharacters
