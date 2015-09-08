1c8bca9578709791bed98091790e65f2db2af54a
Nezhdetov <gyokan.nezhdetov@gmail.com>
2015-09-08 22:16:28 +0300

    Operators and Expressions | Nakov's book
    add new tasks

---
+++ 1.Fundamental-Level/5.Java-Basics/09.Nakov-Book-Exercises/Chapter_3_Operators-and-Expressions/src/_03_CheckDigits.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class _03_CheckDigits {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a number.");
+		int number = scanner.nextInt();
+		boolean isThirdDigitSeven = number % 10 == 7;
+
+		System.out.println("Is third digit seven ? -> " + isThirdDigitSeven);
+	}
+}
---
+++ 1.Fundamental-Level/5.Java-Basics/09.Nakov-Book-Exercises/Chapter_3_Operators-and-Expressions/src/_04_ChekThirdBit.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class _04_ChekThirdBit {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a number.");
+		int number = scanner.nextInt();
+		System.out.println("The number in binary");
+		System.out.println(Integer.toBinaryString(number));
+		boolean isThirdBitOne = ((number >> 2) & 1) == 1;
+		
+		System.out.println("Is third bit one ? -> " + isThirdBitOne);
+	}
+}
