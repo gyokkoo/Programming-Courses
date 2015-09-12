a25a15b3eb02edd1c02f7c44e5fba46ba6e4d17b
Nezhdetov <gyokan.nezhdetov@gmail.com>
2015-09-12 23:01:57 +0300

    Exam Preparation 31 May Exam | in progress
    //TODO

---
+++ 1.Fundamental-Level/1.Advanced-C#/00.Exam-Preparation/Exam-31-May-2015/1.CommandInterpreter/CommandInterpreter.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+class CommandInterpreter
+{
+    static void Main()
+    {
+        char[] emptyEntries = new char[] { ' ' };
+        List<string> collections = Console.ReadLine().Split(emptyEntries, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+        string commandLine = Console.ReadLine();
+
+        while (commandLine != "end")
+        {
+            string[] commandArr = commandLine.Split(emptyEntries, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string command = commandArr[0]; 
+            if(command == "reverse")
+            {
+                int start = int.Parse(commandArr[2]);
+                int count = int.Parse(commandArr[4]);
+
+                if (start >= collections.Count || count > collections.Count || start < 0 || count < 0 || start + count > collections.Count)
+                {
+                    Console.WriteLine("Invalid input parameters.");
+                }
+                else
+                {
+                    collections.Reverse(start, count);
+                }
+            }
+            else if (command == "sort")
+            {
+                int start = int.Parse(commandArr[2]);
+                int count = int.Parse(commandArr[4]);
+
+                if (start >= collections.Count || count > collections.Count || start < 0 || count < 0 || start + count > collections.Count)
+                {
+                    Console.WriteLine("Invalid input parameters.");
+                }
+                else
+                {
+                    collections.Sort(start, count, StringComparer.InvariantCulture);
+                }
+            }
+            else if (command == "rollLeft")
+            {
+                int count = int.Parse(commandArr[1]);
+                if (count < 0)
+                {
+                    Console.WriteLine("Invalid input parameters.");
+                }
+                else
+                {
+                    rollLeft(collections, count);
+                }
+            }
+            else if (command == "rollRight")
+            {
+                int count = int.Parse(commandArr[1]);
+                if (count < 0)
+                {
+                    Console.WriteLine("Invalid input parameters.");
+                }
+                else
+                {
+                    rollRight(collections, count);
+                }
+            }
+
+            commandLine = Console.ReadLine();
+        }
+
+        Console.WriteLine("[" + string.Join(", " , collections) + "]");
+    }
+
+    static void rollRight(List<string> list, int count)
+    {
+        int numberOfRolls = count % list.Count;
+        var elementsToMove = list.Skip(list.Count - numberOfRolls)
+                                 .Take(numberOfRolls)
+                                 .ToArray();
+        list.InsertRange(0, elementsToMove);
+        list.RemoveRange(list.Count - numberOfRolls, numberOfRolls);
+    }
+
+    static void rollLeft(List<string> list, int count)
+    {
+        int numberOfRolls = count % list.Count;
+        var elementsToMove = list.Take(numberOfRolls).ToArray();
+        list.AddRange(elementsToMove);
+        list.RemoveRange(0, numberOfRolls);
+    }
+}
---
+++ 1.Fundamental-Level/1.Advanced-C#/00.Exam-Preparation/Exam-31-May-2015/2.TargetPractice/TargetPractice.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+class TargetPractice
+{
+    static void Main()
+    {
+        string[] matrixDimensions = Console.ReadLine().Split();
+        string snake = Console.ReadLine();
+        string[] shotParameters = Console.ReadLine().Split();
+
+        int rows = int.Parse(matrixDimensions[0]);
+        int columns = int.Parse(matrixDimensions[1]);
+
+        int targetRow = int.Parse(shotParameters[0]);
+        int targetCol = int.Parse(shotParameters[1]);
+        int shotRadius = int.Parse(shotParameters[2]);
+
+        char[,] matrix = new char[rows, columns];
+
+        FillMatrix(matrix, snake, rows, columns);
+
+        FireAShot(matrix, targetRow, targetCol, shotRadius);
+
+        //TODO
+    }
+
+
+
+    static void FillMatrix(char[,] matrix, string snake, int numberOfRows, int numberOfColumns)
+    {
+        string direction = "right";
+        int snakeChRemainder = 0;
+        for (int row = numberOfRows - 1; row >= 0; row--)
+        {
+            if (direction == "left")
+            {
+                for (int col = numberOfColumns - 1; col >= 0; col--)
+                {
+                    matrix[row, col] = snake[(numberOfColumns - col - 1 + snakeChRemainder) % snake.Length];
+                }
+                snakeChRemainder += numberOfColumns % snake.Length;
+                direction = "right";
+                
+            }
+            else if (direction == "right")
+            {
+                for (int col = 0; col < numberOfColumns; col++)
+                {
+                    matrix[row, col] = snake[(col + snakeChRemainder) % snake.Length];
+                }
+                snakeChRemainder += numberOfColumns % snake.Length;
+                direction = "left";
+            }
+        }
+    }
+    static void FireAShot(char[,] matrix, int targetRow, int targetCol, int shotRadius)
+    {
+        //TODO
+    }
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+
+
+
+        for (int row = rows - 1; row >= 0; row--)
+        {
+            for (int col = 0; col < columns; col++)
+            {
+               if(matrix[row, col] == ' ')
+               {
+                   for (int i = rows - 1; i >= 0; i--)
+                   {
+                       if(matrix[i, col] != ' ')
+                       {
+                           matrix[row, col] = matrix[i, col];
+                           matrix[i, col] = ' ';
+                           break;
+                       }
+                   }
+               }
+            }
+        }
+
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write(matrix[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
