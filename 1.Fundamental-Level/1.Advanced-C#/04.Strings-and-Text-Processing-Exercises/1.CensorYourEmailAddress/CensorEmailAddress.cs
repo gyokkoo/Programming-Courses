b4c79d1721fc9585ddfe500b14ecbb6a7acac317
Nezhdetov <gyokan.nezhdetov@gmail.com>
2015-09-25 19:06:24 +0300

    Strings and Text Processing | Exercises done !
    The only exercise is done.

--- 1.Fundamental-Level/1.Advanced-C#/04.Strings-and-Text-Processing-Exercises/1.CensorYourEmailAddress/CensorEmailAddress.cs
+++ 1.Fundamental-Level/1.Advanced-C#/04.Strings-and-Text-Processing-Exercises/1.CensorYourEmailAddress/CensorEmailAddress.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 /*
+You have some text that contains your email address. You’re sick of spammers, so you want to hide it. ]
+You decide to replace all characters in it with asterisks ('*') except the domain. Assume the email address 
+will always be in format [username]@[domain]; you’ll need to replace username with asterisks of equal 
+length and keep the domain unchanged. 
 Input:
  pesho.peshev@email.bg
 My name is Pesho Peshev. I am from Sofia, my email is: pesho.peshev@email.bg (not pesho.peshev@email.com). Test: pesho.meshev@email.bg, pesho.peshev@email.bg
 Output:
  My name is Pesho Peshev. I am from Sofia, my email is: ************@email.bg (not pesho.peshev@email.com). Test: pesho.meshev@email.bg, ************@email.bg
-
  */
 class CensorEmailAddress
 {
@@ -20,12 +23,11 @@ class CensorEmailAddress
 
         string username = correctEmail.Substring(0, indexOfDomainStart);
         string domain = correctEmail.Substring(indexOfDomainStart, (correctEmail.Length - username.Length));
-       
-        string replacementString = new string('*', username.Length) + domain;
 
-        text = text.Replace(correctEmail, replacementString );
+        string censoredEmail = new string('*', username.Length) + domain;
 
-        Console.WriteLine("\nOutput:");
+        text = text.Replace(correctEmail, censoredEmail);
+
         Console.WriteLine(text);
-    }
+    }   
 }
