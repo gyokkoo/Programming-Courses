67f88af6da4e2a3de0816d928cfc87b3b6636cb5
Nezhdetov <gyokan.nezhdetov@gmail.com>
2015-10-03 23:04:43 +0300

    Streams and Files Homework | in progress
    homework in progress

---
+++ 1.Fundamental-Level/1.Advanced-C#/06.Streams-and-Files-Homework/01.OddLines/OddLines.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+/*
+Write a program that reads a text file and prints on the console its odd lines. 
+Line numbers starts from 0. Use StreamReader
+ */
+class OddLines
+{
+    static void Main()
+    {
+        Console.Title = "Problem 1.	Odd Lines";
+
+        StreamReader reader = new StreamReader("../../text.txt");
+
+        using (reader)
+        {
+            int lineNumber = 0;
+
+            string line = reader.ReadLine();
+
+            while (line != null)
+            {
+                lineNumber++;
+                if (lineNumber%2 == 0)
+                {
+                    Console.WriteLine(line);
+                }
+
+                line = reader.ReadLine();
+            }
+        }
+    }
+}
+
