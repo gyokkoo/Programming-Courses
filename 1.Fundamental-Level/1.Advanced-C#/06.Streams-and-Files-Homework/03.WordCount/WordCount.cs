6f008a5dad762f77cbcbd759ef67d8f2eba9c20b
Nezhdetov <gyokan.nezhdetov@gmail.com>
2015-10-04 14:16:17 +0300

    Streams and Files | Homework done
    todo *tasks

---
+++ 1.Fundamental-Level/1.Advanced-C#/06.Streams-and-Files-Homework/03.WordCount/WordCount.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+/*
+Write a program that reads a list of words from the file words.txt and finds how many times each of the words 
+is contained in another file text.txt. 
+Matching should be case-insensitive.
+Write the results in file results.txt. 
+Sort the words by frequency in descending order. 
+Use StreamReader in combination with StreamWriter.
+ */
+class WordCount
+{
+    static void Main()
+    {
+        Console.Title = "Problem 3.	Word Count";
+
+        var wordsCountDictionary = new Dictionary<string, int>();
+        StreamReader reader = new StreamReader("../../text.txt");
+        char[] punctuationMarks = { ' ', ',', '.', '\r', '-', '\n', '?', '-' };
+        string[] text = reader.ReadToEnd().Split(punctuationMarks, StringSplitOptions.RemoveEmptyEntries);
+        reader.Close();
+
+        StreamReader wordsReader = new StreamReader("../../words.txt");
+        StreamWriter writer = new StreamWriter("../../result.txt");
+
+        using (wordsReader)
+        {
+            string word = wordsReader.ReadLine();
+
+            while (word != null)
+            {
+                wordsCountDictionary[word] = text.Count(x => x.ToLower() == word);
+                word = wordsReader.ReadLine();
+            }
+        }
+
+        using (writer)
+        {
+            foreach (var pair in wordsCountDictionary.OrderByDescending(x => x.Value))
+            {
+                writer.WriteLine("{0} - {1}", pair.Key, pair.Value);
+                Console.WriteLine("{0} - {1}", pair.Key, pair.Value);
+            }
+        }
+    }
+}
---
+++ 1.Fundamental-Level/1.Advanced-C#/06.Streams-and-Files-Homework/04.CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+/*
+Write a program that copies the contents of a binary file (e.g. image, video, etc.) to another using FileStream. 
+You are not allowed to use the File class or similar helper classes.
+ */
+
+class CopyBinaryFile
+{
+    static void Main()
+    {
+        Console.Title = "Problem 4.	Copy Binary File";
+
+        using (var source = new FileStream("../../picture.jpg.", FileMode.Open))
+        {
+            using (var destination = new FileStream("../../result.jpg", FileMode.Create))
+            {
+                double fileLength = source.Length;
+                byte[] buffer = new byte[4096];
+                while (true)
+                {
+                    int readBytes = source.Read(buffer, 0, buffer.Length);
+                    if (readBytes == 0)
+                    {
+                        break;
+                    }
+
+                    destination.Write(buffer, 0, readBytes);
+
+                    Console.WriteLine("{0:P}", Math.Min(source.Position / fileLength, 1));
+                }
+            }
+        }
+    }
+}
