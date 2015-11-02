07f3d05c5b59d5657f7dafb24f42814eb7b2a08a
Gyokan <gyokan.nezhdetov@gmail.com>
2015-11-02 13:50:17 +0200

    update LogsAggregator.java

--- 1.Fundamental-Level/2.Java-Fundamentals/00.Exam-Preparation/Exam-1-June-2014/src/_4_LogsAggregator.java
+++ 1.Fundamental-Level/2.Java-Fundamentals/00.Exam-Preparation/Exam-1-June-2014/src/_4_LogsAggregator.java
@@ -7,33 +7,33 @@ public class _4_LogsAggregator {
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
 
-        TreeMap<String, Integer> durations = new TreeMap<>();
-        HashMap<String, TreeSet<String>> ipAddresses = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            String[] information = scanner.nextLine().split(" ");
-            String ip = information[0];
-            String user = information[1];
-            int duration = Integer.parseInt(information[2]);
 
-            Integer oldDuration = durations.get(user);
-            if(oldDuration == null) {
-                oldDuration = 0;
+        TreeMap<String, Integer> durations = new TreeMap<>();
+        TreeMap<String, TreeSet<String>> ipAddresses = new TreeMap<>();
+        for (int i = 0; i < n; i++) {
+            String[] lineArgs = scanner.nextLine().split(" ");
+            String user = lineArgs[1];
+            String ipAddress = lineArgs[0];
+            int duration = Integer.parseInt(lineArgs[2]);
+
+            if(!durations.containsKey(user)) {
+                durations.put(user, duration);
+            } else {
+                duration += durations.get(user);
+                durations.put(user, duration);
             }
-            durations.put(user, oldDuration + duration);
 
             TreeSet<String> ipSet = ipAddresses.get(user);
             if(ipSet == null) {
                 ipSet = new TreeSet<>();
             }
-            ipSet.add(ip);
+            ipSet.add(ipAddress);
             ipAddresses.put(user, ipSet);
         }
 
-        for (Map.Entry<String, Integer> userAndDuration : durations.entrySet()) {
-            String user = userAndDuration.getKey();
-            int duration = userAndDuration.getValue();
-            TreeSet<String> ipSet = ipAddresses.get(user);
-            System.out.printf("%s: %d %s%n", user, duration, ipSet);
+        for (Map.Entry<String, Integer> entry : durations.entrySet()) {
+            System.out.printf("%s: %d ", entry.getKey(), entry.getValue());
+            System.out.println(ipAddresses.get(entry.getKey()));
         }
     }
 }
