dcc20d8fdf8f0befc6b7f698bbe2e1d75b868096
Gyokan <gyokan.nezhdetov@gmail.com>
2015-11-22 19:58:00 +0200

    Defining classes | Exercise
    added first topic exercise

---
+++ 1.Fundamental-Level/3.OOP/01.Defining-Classes-Exercise/DefiningClasses-Exercise/DefiningClasses-Exercise.cs
@@ -0,0 +1,22 @@
+﻿using DefiningClasses_Exercise;
+using System;
+
+class DefiningClasses
+{
+    static void Main(string[] args)
+    {
+        Dog unnamed = new Dog();
+        Dog sharo = new Dog("Sharo", "Melez");
+        Dog dogi = new Dog("Dogi", "Shepherd");
+
+        Dog[] dogs = new Dog[3];
+        dogs[0] = sharo;
+        dogs[1] = dogi;
+        dogs[2] = unnamed;
+
+        foreach (Dog dog in dogs)
+        {
+            dog.Bark();
+        }
+    }
+}
---
+++ 1.Fundamental-Level/3.OOP/01.Defining-Classes-Exercise/DefiningClasses-Exercise/Dog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DefiningClasses_Exercise
+{
+    public class Dog
+    {
+        public Dog()
+            : this(null, null)
+        {
+
+        }
+
+        public Dog(string name, string breed)
+        {
+            this.Name = name;
+            this.Breed = breed;
+        }
+
+        public string Name { get; set; }
+
+        public string Breed { get; set; }
+
+        public void Bark()
+        {
+            Console.WriteLine(
+                "{0} ({1}) said: Bauuuuuu!", 
+                this.Name ?? "[unnamed dog]",
+                this.Breed ?? "[unknown breed]");
+        }
+    }
+}
