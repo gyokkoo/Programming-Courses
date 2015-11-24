3d454b9e80a675a18f3e25913e3a1f274d364262
Gyokan <gyokan.nezhdetov@gmail.com>
2015-11-24 22:12:21 +0200

    Exception Handling | Exercise
    Added exercise on exception handling

---
+++ 1.Fundamental-Level/3.OOP/02.Exception-Handling-Exercise/Exception-Handling-Exercise/Person.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public class Person
+{
+    private string firstName;
+    private string lastName;
+    private int age;
+
+    public Person(string firstName, string lastName, int age)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Age = age;
+    }
+
+    public string FirstName
+    {
+        get { return this.firstName; }
+
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException("value", "The first name cannot be null or empty.");
+            }
+
+            this.firstName = value;
+        }
+    }
+
+    public string LastName
+    {
+        get { return this.lastName; }
+
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("value", "The last name cannot be null or empty.");
+            }
+
+            this.lastName = value;
+        }
+    }
+
+    public int Age
+    {
+        get { return this.age; }
+
+        set
+        {
+            if (value < 0 || 120 < value)
+            {
+                throw new ArgumentOutOfRangeException("value", "Age should be in the range [0...120].");
+            }
+
+            this.age = value;
+        }
+    }
+}
+
---
+++ 1.Fundamental-Level/3.OOP/02.Exception-Handling-Exercise/Exception-Handling-Exercise/PersonsMain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class PersonsMain
+{
+    static void Main()
+    {
+        Person sasho = new Person("Sasho", "Ivanov", 25);
+        
+        try
+        {
+            Person noName = new Person(string.Empty, "Goshev", 31);
+        }
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine("Exception thrown: {0}", ex.Message);
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            Console.WriteLine("Exception thrown: {0}", ex.Message);
+        }
+    
+        //try
+        //{
+        //    Person negativeAge = new Person("Kaloyan", "Ivanov", -4);
+        //}
+        //catch (ArgumentNullException ex)
+        //{
+        //    Console.WriteLine("Exception thrown: {0}", ex.Message);
+        //}
+        //catch (ArgumentOutOfRangeException ex)
+        //{
+        //    Console.WriteLine("Exception thrown: {0}", ex.Message);
+        //}
+    }
+}
+
