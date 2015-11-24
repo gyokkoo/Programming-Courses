73a6e3e09cc04235d80fd52878a9e68e34b8531f
Gyokan <gyokan.nezhdetov@gmail.com>
2015-11-24 23:36:40 +0200

    Exception Handling | Homework
    Added exception handling homework.

---
+++ 1.Fundamental-Level/3.OOP/02.Exception-Handling-Homework/01.SquareRoot/SquareRoot.cs
@@ -0,0 +1,53 @@
+﻿using System;
+/*
+Write a program that reads an integer number and calculates and prints its square root. 
+If the number is invalid or negative, print "Invalid number". 
+In all cases finally print "Good bye". Use try-catch-finally.
+*/
+
+public class SquareRoot
+{
+    static void Main()
+    {
+        Console.Title = "Problem 1.	Square Root";
+
+        Console.Write("Enter an integer number: ");
+        string inputNumber = Console.ReadLine();
+
+        try
+        {
+            int number = int.Parse(inputNumber);
+            int squareRoot = Sqrt(number);
+            Console.WriteLine("sqrt({0}) = {1}", number, squareRoot);
+        }
+        catch (FormatException fe)
+        {
+            Console.Error.WriteLine(fe.Message);
+            //throw fe;
+        }
+        catch (ArgumentOutOfRangeException re)
+        {
+            Console.Error.WriteLine(re.Message);
+            //throw re;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Invalid number!");
+        }
+        finally
+        {
+            Console.WriteLine("Good bye");
+        }
+    }
+
+    private static int Sqrt(int value)
+    {
+        if (value < 0)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+        int sqrtNumber = (int)Math.Sqrt(value);
+
+        return sqrtNumber;
+    }
+}
---
+++ 1.Fundamental-Level/3.OOP/02.Exception-Handling-Homework/02.EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,56 @@
+﻿using System;
+/*
+Write a method ReadNumber(int start, int end) that enters an integer number in given range [start…end]. 
+If an invalid number or non-number text is entered, the method should throw an exception. 
+Using this method write a program that enters 10 numbers: a1, a2, … a10, such that 1 < a1 < … < a10 < 100. 
+If the user enters an invalid number, let the user to enter again.
+*/
+public class EnterNumbers
+{
+    static void Main()
+    {
+        Console.Title = "Problem 2.	Enter Numbers";
+
+        int start = 1;
+        int end = 100;
+
+        Console.WriteLine("Etner 10 numbers in range [{0}...{1}]", start, end);
+        for (int i = 0; i < 10; i++)
+        {
+            try
+            {
+                Console.Write("Enter number {0} -> ", i + 1);
+                ReadNumber(start, end);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("The number format is not correct! \nPlease try again.");
+                i--;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("The number is not in the given range {0} < number < {1}! \nPlease try again.",
+                    start, end);
+                i--;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid number! \nPlease try again.");
+                i--;
+            }
+        }
+
+        Console.WriteLine("Congratulations! You successfully entered 10 numbers in range [{0}...{1}]",
+            start, end);
+    }
+
+    private static void ReadNumber(int start, int end)
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        if (number < start || number > end)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+    }
+}
