404d23ff23b2ab8aff026c66b5d9dd3b1580c599
Gyokan <gyokan.nezhdetov@gmail.com>
2015-12-08 23:22:13 +0200

    Other Types | Exercise
    Added other types in OOP exercise

---
+++ 1.Fundamental-Level/3.OOP/00.OOP-Exercises/09.OtherTypes/CustomList.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Linq;
+
+namespace _09.OtherTypes
+{
+    public class CustomList<T> where T : IComparable<T>
+    {
+        const int DefaultCapacity = 8;
+        private T[] elements;
+        private int currentIndex;
+
+        public CustomList(int initialCapacity = DefaultCapacity)
+        {
+            this.elements = new T[initialCapacity];
+            this.currentIndex = 0;
+        }
+
+        public void Add(T elementToAdd)
+        {
+            if (this.currentIndex >= this.elements.Length)
+            {
+                this.Resize();
+            }
+
+            this.elements[this.currentIndex] = elementToAdd;
+            this.currentIndex++;
+        }
+
+        private void Resize()
+        {
+            var newElements = new T[this.elements.Length * 2];
+
+            for (int i = 0; i < this.currentIndex; i++)
+            {
+                newElements[i] = this.elements[i];
+            }
+
+            this.elements = newElements;
+        }
+
+        public int IndexOf(T elementToFind)
+        {
+            if (this.currentIndex == 0)
+            {
+                throw new ArgumentException("The list is empty.");
+            }
+
+            for (int i = 0; i < this.currentIndex; i++)
+            {
+                if (this.elements[i].Equals(elementToFind))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Remove(T elementToRemove)
+        {
+            int index = this.IndexOf(elementToRemove);
+
+            if (index == -1)
+            {
+                throw new ArgumentException("Specified element was not found.");
+            }
+
+            for (int i = index; i < this.currentIndex - 1; i++)
+            {
+                this.elements[i] = this.elements[i + 1];
+            }
+
+            this.currentIndex--;
+            this.elements[currentIndex] = default(T);
+        }
+
+        public T Max()
+        {
+            if (this.currentIndex == 0)
+            {
+                throw new ArgumentException("The list is empty.");
+            }
+
+            T maxElement = this.elements[0];
+
+            for (int i = 0; i < this.currentIndex; i++)
+            {
+                if (maxElement.CompareTo(this.elements[i]) < 0)
+                {
+                    maxElement = this.elements[i];
+                }
+            }
+
+            return maxElement;
+        }
+
+        public T Min()
+        {
+            if (this.currentIndex == 0)
+            {
+                throw new ArgumentException("The list is empty.");
+            }
+
+            T minElement = this.elements[0];
+
+            for (int i = 0; i < this.currentIndex; i++)
+            {
+                if (minElement.CompareTo(this.elements[i]) > 0)
+                {
+                    minElement = this.elements[i];
+                }
+            }
+
+            return minElement;
+        }
+
+        public T this[int i]
+        {
+            get
+            {
+                if (i < 0 || i >= this.currentIndex)
+                {
+                    throw new IndexOutOfRangeException("Index was outside the boundaries of the CustomList.");
+                }
+
+                return this.elements[i];
+            }
+
+            set
+            {
+                if (i < 0 || i >= this.currentIndex)
+                {
+                    throw new IndexOutOfRangeException("Index was outside the boundaries of the CustomList.");
+                }
+
+                this.elements[i] = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[{0}]", string.Join(", ", this.elements.Take(currentIndex)));
+        }
+    }
+}
---
+++ 1.Fundamental-Level/3.OOP/00.OOP-Exercises/09.OtherTypes/OtherTypesExercise.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _09.OtherTypes
+{
+    public class OtherTypesExercise
+    {
+        public static void Main()
+        {
+            var list = new CustomList<int>();
+
+            list.Add(0);
+            list.Add(5);
+            list.Add(-1);
+            list.Add(101);
+
+            Console.WriteLine(list.Max());
+            Console.WriteLine(list.Min());
+
+            Console.WriteLine(list[0]);
+            Console.WriteLine(list[1]);
+            Console.WriteLine(string.Join(", ", list));
+        }
+    }
+}
