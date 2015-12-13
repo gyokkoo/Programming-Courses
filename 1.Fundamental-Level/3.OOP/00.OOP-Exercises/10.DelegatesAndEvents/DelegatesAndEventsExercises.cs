c34c7402fd835bd0a11023ca1b465aa436666941
Gyokan <gyokan.nezhdetov@gmail.com>
2015-12-13 22:29:41 +0200

    Delegates and Events | Exercise
    Added delegates and events OOP course exercise

---
+++ 1.Fundamental-Level/3.OOP/00.OOP-Exercises/10.DelegatesAndEvents/DelegatesAndEventsExercises.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10.DelegatesAndEvents
+{
+    public class DelegatesAndEventsExercises
+    {
+        public static void Main()
+        {
+            List<int> collection = new List<int> { 1, 2, 3, 4, 6, 11, 3 };
+
+            Console.WriteLine(collection.FirstOrDefault(x => x > 7));
+            Console.WriteLine(collection.FirstOrDefault(x => x < 0));
+
+            Console.WriteLine(string.Join(", ", collection.TakeWhile(x => x < 10)));
+
+            collection.ForEach(Console.WriteLine);
+        }
+    }
+}
---
+++ 1.Fundamental-Level/3.OOP/00.OOP-Exercises/10.DelegatesAndEvents/Extensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10.DelegatesAndEvents
+{
+    public static class Extensions
+    {
+        public static T FirstOrDefault<T>(this IEnumerable<T> collection, Predicate<T> predicate)
+        {
+            foreach (var element in collection)
+            {
+                if (predicate(element))
+                {
+                    return element;
+                }
+            }
+
+            return default(T);
+        }
+
+        public static IEnumerable<T> TakeWhile<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            var result = new List<T>();
+
+            foreach (var element in collection)
+            {
+                if (predicate(element))
+                {
+                    result.Add(element);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> collection, Action<T> action)
+        {
+            foreach (var element in collection)
+            {
+                action(element);
+            }
+        }
+    }
+}
