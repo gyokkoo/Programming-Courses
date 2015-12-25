dda258443dc4dc9e62835c99b392689545e0892b
Gyokan <gyokan.nezhdetov@gmail.com>
2015-12-26 00:34:14 +0200

    Exam Problems Practice | add new problems
    20th and 21st problems are solved.

---
+++ 1.Fundamental-Level/1.Advanced-C#/00.Exam-Preparation/Exam-Problems-Practice/20.OhMyGirl/OhMyGirl.cs
@@ -0,0 +1,94 @@
+﻿namespace _20.OhMyGirl
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class OhMyGirl
+    {
+        public static void Main()
+        {
+            string key = Console.ReadLine();
+            string line = Console.ReadLine();
+
+            StringBuilder text = new StringBuilder();
+            while (line != "END")
+            {
+                line = Regex.Replace(line, "\\s+", " ");
+                text.Append(line);
+                line = Console.ReadLine();
+            }
+
+            string pattern = BuildPattern(key) + "(.{2,6}?)" + BuildPattern(key);
+            MatchCollection matches = Regex.Matches(text.ToString(), pattern);
+            foreach (Match match in matches)
+            {
+                Console.Write(match.Groups[1]);
+            }
+        }
+
+        private static string BuildPattern(string key)
+        {
+            const string digitRegex = "[0-9]*";
+            const string lowerLetterRegex = "[a-z]*";
+            const string upperLetterRegex = "[A-Z]*";
+
+            StringBuilder pattern = new StringBuilder();
+            if (IsSpecial(key[0]))
+            {
+                pattern.Append("\\" + key[0]);
+            }
+            else
+            {
+                pattern.Append(key[0]);
+            }
+
+            for (int i = 1; i < key.Length - 1; i++)
+            {
+                if (char.IsDigit(key[i]))
+                {
+                    pattern.Append(digitRegex);
+                }
+                else if (char.IsLower(key[i]))
+                {
+                    pattern.Append(lowerLetterRegex);
+                }
+                else if (char.IsUpper(key[i]))
+                {
+                    pattern.Append(upperLetterRegex);
+                }
+                else if (IsSpecial(key[i]))
+                {
+                    pattern.Append("\\" + key[i]);
+                }
+                else
+                {
+                    pattern.Append(key[i]);
+                }
+            }
+
+            if (IsSpecial(key[key.Length - 1]))
+            {
+                pattern.Append("\\" + key[key.Length - 1]);
+            }
+            else
+            {
+                pattern.Append(key[key.Length - 1]);
+            }
+
+            return pattern.ToString(); ;
+        }
+
+        private static bool IsSpecial(char c)
+        {
+            if (c == '[' || c == ']' || c == '(' || c == ')' ||
+                c == '+' || c == '.' || c == '*')
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ 1.Fundamental-Level/1.Advanced-C#/00.Exam-Preparation/Exam-Problems-Practice/21.ITVillage/ITVillage.cs
@@ -0,0 +1,152 @@
+﻿namespace _21.ITVillage
+{
+    using System;
+    using System.Linq;
+
+    public class ItVillage
+    {
+        private const int Rows = 4;
+        private const int Columns = 4;
+
+        public static void Main()
+        {
+            string[,] board = new string[Rows, Columns];
+            string[] boardInfo = Console.ReadLine().Trim().Replace(" | ", " ").Split();
+            string[] startCoordinates = Console.ReadLine().Split();
+            int[] diceNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int startRow = int.Parse(startCoordinates[0]) - 1;
+            int startColumn = int.Parse(startCoordinates[1]) - 1;
+            int currentRow = startRow;
+            int currentCol = startColumn;
+
+            int coins = 50;
+
+            FillBoard(board, boardInfo);
+
+            string gameResult = GetGameResult(diceNumbers, board, currentRow, currentCol, coins);
+
+            Console.WriteLine($"<p>{gameResult}<p>");
+
+        }
+
+        private static string GetGameResult(int[] diceNumbers, string[,] board, int currentRow, int currentCol, int coins)
+        {
+            int innCount = 0;
+
+            for (int i = 0; i < diceNumbers.Length; i++)
+            {
+                coins += innCount * 20;
+
+                int diceNumber = diceNumbers[i] % 12;
+
+                string fieldLetter = GetNextFieldLetter(diceNumber, board, ref currentRow, ref currentCol);
+
+                switch (fieldLetter)
+                {
+                    case "P":
+                        coins -= 5;
+                        break;
+                    case "I":
+                        if (coins < 100)
+                        {
+                            coins -= 10;
+                        }
+                        else
+                        {
+                            coins -= 100;
+                            board[currentRow, currentCol] = "Home";
+                            innCount++;
+                        }
+                        break;
+                    case "F":
+                        coins += 20;
+                        break;
+                    case "S":
+                        i += 2;
+                        coins -= 2 * innCount * 20;
+                        break;
+                    case "V":
+                        coins *= 10;
+                        break;
+                    case "Home":
+                        break;
+                    case "N":
+                        return "You won! Nakov's force was with you!";
+                    default:
+                        throw new ArgumentException("Invalid field!");
+                }
+
+                if (HasBoughtAllInns(board))
+                {
+                    return $"You won! You own the village now! You have {coins} coins!";
+                }
+
+                if (coins < 0)
+                {
+                    return "You lost! You ran out of money!";
+                }
+            }
+
+
+            return $"You lost! No more moves! You have {coins} coins!";
+        }
+
+        private static bool HasBoughtAllInns(string[,] board)
+        {
+            for (int row = 0; row < Rows; row++)
+            {
+                for (int col = 0; col < Columns; col++)
+                {
+                    if (board[row, col] == "I")
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private static string GetNextFieldLetter(int diceNumber, string[,] board, ref int currentRow, ref int currentCol)
+        {
+            while (diceNumber != 0)
+            {
+                if (currentRow == 0 && currentCol + 1 < Columns)
+                {
+                    currentCol++;
+                }
+                else if (currentCol == Columns - 1 && currentRow + 1 < Rows)
+                {
+                    currentRow++;
+                }
+                else if (currentRow == Rows - 1 && currentCol - 1 >= 0)
+                {
+                    currentCol--;
+                }
+                else if (currentCol == 0 && currentRow - 1 >= 0)
+                {
+                    currentRow--;
+                }
+
+                diceNumber--;
+            }
+
+            string letter = board[currentRow, currentCol];
+            return letter;
+        }
+
+        private static void FillBoard(string[,] board, string[] boardInfo)
+        {
+            int index = 0;
+            for (int row = 0; row < Rows; row++)
+            {
+                for (int col = 0; col < Columns; col++)
+                {
+                    board[row, col] = boardInfo[index];
+                    index++;
+                }
+            }
+        }
+    }
+}
