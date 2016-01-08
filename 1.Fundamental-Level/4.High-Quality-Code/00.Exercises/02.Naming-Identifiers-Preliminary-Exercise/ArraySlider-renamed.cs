74106d335b4aed85dd31b92dde4d13474227a8f8
Gyokan <gyokan.nezhdetov@gmail.com>
2016-01-08 15:36:20 +0200

    Naming Identifiers | Exercise
    Added naming identifiers preliminary exercise.

---
+++ 1.Fundamental-Level/4.High-Quality-Code/00.Exercises/02.Naming-Identifiers-Preliminary-Exercise/ArraySlider-renamed.cs
@@ -0,0 +1,75 @@
+﻿namespace ArraySlider_MySolution
+{
+    using System;
+    using System.Linq;
+    using System.Numerics;
+
+    public class ArraySlider
+    {
+        public static void Main()
+        {
+            BigInteger[] numbers = Console.ReadLine()
+                .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(BigInteger.Parse)
+                .ToArray();
+
+            string command = Console.ReadLine();
+            long currentIndex = 0;
+            while (command != "stop")
+            {
+                string[] commandParams = command.Split(' ');
+                long offset = long.Parse(commandParams[0]) % numbers.Length;
+                string operation = commandParams[1];
+                long operand = long.Parse(commandParams[2]);
+
+                if (offset < 0)
+                {
+                    offset += numbers.Length;
+                }
+
+                currentIndex = (currentIndex + offset) % numbers.Length;
+
+                PerformOperation(operation, numbers, currentIndex, operand);
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine("[{0}]", string.Join(", ", numbers));
+        }
+
+        private static void PerformOperation(string operation, BigInteger[] numbers, long currentIndex, long operand)
+        {
+            switch (operation)
+            {
+                case "&":
+                    numbers[currentIndex] &= operand;
+                    break;
+                case "|":
+                    numbers[currentIndex] |= operand;
+                    break;
+                case "^":
+                    numbers[currentIndex] ^= operand;
+                    break;
+                case "+":
+                    numbers[currentIndex] += operand;
+                    break;
+                case "-":
+                    numbers[currentIndex] -= operand;
+                    break;
+                case "*":
+                    numbers[currentIndex] *= operand;
+                    break;
+                case "/":
+                    numbers[currentIndex] /= operand;
+                    break;
+                default:
+                    throw new ArgumentException("Unknown command.");
+            }
+
+            if (numbers[currentIndex] < 0)
+            {
+                numbers[currentIndex] = BigInteger.Zero;
+            }
+        }
+    }
+}
