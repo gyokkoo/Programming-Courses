744bf1e2a5035e4b45a224c6278003c588c0a887
Gyokan <gyokan.nezhdetov@gmail.com>
2016-01-30 17:27:52 +0200

    Debugging Lab | Exam Preparation
    Added debugging lab.

---
+++ 1.Fundamental-Level/4.High-Quality-Code/00.Exam-Preparation/Debugging-Lab/01.InstructionSet/InstructionSet.cs
@@ -0,0 +1,61 @@
+﻿namespace _01.InstructionSet
+{
+    using System;
+    using System.Numerics;
+
+    public class InstructionSet
+    {
+        public static void Main()
+        {
+            string line = Console.ReadLine();
+
+            while (line != "END")
+            {
+                string[] codeArgs = line.Split(' ');
+
+                BigInteger result = 0;
+                switch (codeArgs[0])
+                {
+                    case "INC":
+                        {
+                            long operandOne = long.Parse(codeArgs[1]);
+                            result = operandOne + 1;
+                            break;
+                        }
+
+                    case "DEC":
+                        {
+                            long operandOne = long.Parse(codeArgs[1]);
+                            result = operandOne - 1;
+                            break;
+                        }
+
+                    case "ADD":
+                        {
+                            long operandOne = long.Parse(codeArgs[1]);
+                            long operandTwo = long.Parse(codeArgs[2]);
+                            result = operandOne + operandTwo;
+                            break;
+                        }
+
+                    case "MLA":
+                        {
+                            long operandOne = long.Parse(codeArgs[1]);
+                            long operandTwo = long.Parse(codeArgs[2]);
+                            result = operandOne * operandTwo;
+                            break;
+                        }
+
+                    default:
+                        {
+                            throw new InvalidOperationException("Unknown command.");
+                        }
+                }
+
+                Console.WriteLine(result);
+
+                line = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ 1.Fundamental-Level/4.High-Quality-Code/00.Exam-Preparation/Debugging-Lab/03.ArrayTest/ArrayTest.cs
@@ -0,0 +1,103 @@
+﻿namespace _03.ArrayTest
+{
+    using System;
+    using System.Linq;
+
+    public class ArrayTest
+    {
+        private const char ArgumentsDelimiter = ' ';
+
+        public static void Main()
+        {
+            int sizeOfArray = int.Parse(Console.ReadLine());
+
+            long[] array = Console.ReadLine()
+                .Split(ArgumentsDelimiter)
+                .Select(long.Parse)
+                .ToArray();
+
+            string command = Console.ReadLine();
+
+            while (!command.Equals("stop"))
+            {
+                string[] stringParams = command.Split(ArgumentsDelimiter);
+                int[] args = new int[2];
+
+                string commandType = stringParams[0];
+                if (commandType.Equals("add") ||
+                    commandType.Equals("subtract") ||
+                    commandType.Equals("multiply"))
+                {
+                    args[0] = int.Parse(stringParams[1]) - 1;
+                    args[1] = int.Parse(stringParams[2]);
+
+                    PerformAction(array, commandType, args);
+                }
+                else if (stringParams[0].Equals("lshift"))
+                {
+                    ArrayShiftLeft(array);
+                }
+                else if (stringParams[0].Equals("rshift"))
+                {
+                    ArrayShiftRight(array);
+                }
+
+                PrintArray(array);
+                Console.WriteLine();
+
+                command = Console.ReadLine();
+            }
+        }
+
+        private static void PerformAction(long[] arr, string action, int[] args)
+        {
+            int pos = args[0];
+            int value = args[1];
+
+            switch (action)
+            {
+                case "multiply":
+                    arr[pos] *= value;
+                    break;
+                case "add":
+                    arr[pos] += value;
+                    break;
+                case "subtract":
+                    arr[pos] -= value;
+                    break;
+                default:
+                    throw new InvalidOperationException("Invalid command.");
+            }
+        }
+
+        private static void ArrayShiftRight(long[] array)
+        {
+            long lastElement = array[array.Length - 1];
+            for (int i = array.Length - 1; i >= 1; i--)
+            {
+                array[i] = array[i - 1];
+            }
+
+            array[0] = lastElement;
+        }
+
+        private static void ArrayShiftLeft(long[] array)
+        {
+            long firstElement = array[0];
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+
+            array[array.Length - 1] = firstElement;
+        }
+
+        private static void PrintArray(long[] array)
+        {
+            foreach (long number in array)
+            {
+                Console.Write(number + " ");
+            }
+        }
+    }
+}
