1f017b48ebc87fb85277454dd6cb7b6b937c5bb2
Gyokan <gyokan.nezhdetov@gmail.com>
2016-01-30 17:06:25 +0200

    Refactoring | Homework
    Added refactoring homework.

---
+++ 1.Fundamental-Level/4.High-Quality-Code/17.Refactoring-Homework/RoatingWalkInMatrix.Tests/RotatingWalkInMatrixTests.cs
@@ -0,0 +1,75 @@
+﻿namespace RotingWalkInMatrix.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using RotatingWalkInMatrix;
+
+    [TestClass]
+    public class RotatingWalkInMatrixTests
+    {
+        [TestMethod]
+        public void TestGenerateMatrix_Size1()
+        {
+            int[,] matrix = WalkInMatrix.GenerateMatrix(6);
+
+            int[,] expectedMatrix = { { 1 } };
+
+            for (int row = 0; row < expectedMatrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < expectedMatrix.GetLength(1); col++)
+                {
+                    Assert.AreEqual(matrix[row, col], expectedMatrix[row, col]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestGenerateMatrix_Size2()
+        {
+            int[,] matrix = WalkInMatrix.GenerateMatrix(2);
+
+            int[,] expectedMatrix = { { 1, 4 }, { 3, 2 } };
+
+            for (int row = 0; row < expectedMatrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < expectedMatrix.GetLength(1); col++)
+                {
+                    Assert.AreEqual(matrix[row, col], expectedMatrix[row, col]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestGenerateMatrix_Size3()
+        {
+            int[,] matrix = WalkInMatrix.GenerateMatrix(3);
+
+            int[,] expectedMatrix =
+                {
+                    { 1, 7, 8 },
+                    { 6, 2, 9 },
+                    { 5, 4, 3 }
+                };
+        }
+
+        [TestMethod]
+        public void TestGenerateMatrix_Size6()
+        {
+            int[,] matrix = WalkInMatrix.GenerateMatrix(6);
+
+            int[,] expectedMatrix =
+                {
+                    { 1, 16, 17, 18, 19, 20 }, { 15, 2, 27, 28, 29, 21 }, { 14, 31, 3, 26, 30, 22 },
+                    { 13, 36, 32, 4, 25, 23 }, { 12, 35, 34, 33, 5, 24 }, { 11, 10, 9, 8, 7, 6 }
+                };
+
+            for (int row = 0; row < expectedMatrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < expectedMatrix.GetLength(1); col++)
+                {
+                    Assert.AreEqual(matrix[row, col], expectedMatrix[row, col]);
+                }
+            }
+        }
+    }
+}
---
+++ 1.Fundamental-Level/4.High-Quality-Code/17.Refactoring-Homework/RotatingWalkInMatrix/WalkInMatrix.cs
@@ -0,0 +1,139 @@
+﻿namespace RotatingWalkInMatrix
+{
+    using System;
+
+    public class WalkInMatrix
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = GenerateMatrix(n);
+
+            PrintMatrix(matrix);
+        }
+
+        public static int[,] GenerateMatrix(int n)
+        {
+            int[,] matrix = new int[n, n];
+            int currentNumber = 1;
+            int x = 0;
+            int y = 0;
+            int dirX = 1;
+            int dirY = 1;
+
+            while (currentNumber <= n * n)
+            {
+                matrix[x, y] = currentNumber;
+
+                if (AdjacentCellsNotEmpty(matrix, x, y))
+                {
+                    SetPositionToEmptyCell(matrix, out x, out y);
+                    dirX = 1;
+                    dirY = 1;
+                    currentNumber++;
+                    continue;
+                }
+
+                while (x + dirX >= n || x + dirX < 0 || y + dirY >= n || y + dirY < 0 || matrix[x + dirX, y + dirY] != 0)
+                {
+                    ChangeDirection(ref dirX, ref dirY);
+                }
+
+                x += dirX;
+                y += dirY;
+                currentNumber++;
+            }
+
+            return matrix;
+        }
+
+        private static void ChangeDirection(ref int dX, ref int dY)
+        {
+            int[] directionsX = { 1, 1, 1, 0, -1, -1, -1, 0 };
+            int[] directionsY = { 1, 0, -1, -1, -1, 0, 1, 1 };
+
+            int currentDirectionIndex = 0;
+            for (int count = 0; count < 8; count++)
+            {
+                if (directionsX[count] == dX && directionsY[count] == dY)
+                {
+                    currentDirectionIndex = count;
+                    break;
+                }
+            }
+
+            if (currentDirectionIndex == 7)
+            {
+                dX = directionsX[0];
+                dY = directionsY[0];
+            }
+            else
+            {
+                dX = directionsX[currentDirectionIndex + 1];
+                dY = directionsY[currentDirectionIndex + 1];
+            }
+        }
+
+        private static bool AdjacentCellsNotEmpty(int[,] arr, int x, int y)
+        {
+            int[] dirX = { 1, 1, 1, 0, -1, -1, -1, 0 };
+            int[] dirY = { 1, 0, -1, -1, -1, 0, 1, 1 };
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (x + dirX[i] >= arr.GetLength(0) || x + dirX[i] < 0)
+                {
+                    dirX[i] = 0;
+                }
+
+                if (y + dirY[i] >= arr.GetLength(1) || y + dirY[i] < 0)
+                {
+                    dirY[i] = 0;
+                }
+            }
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (arr[x + dirX[i], y + dirY[i]] == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static void SetPositionToEmptyCell(int[,] arr, out int x, out int y)
+        {
+            x = 0;
+            y = 0;
+
+            for (int row = 0; row < arr.GetLength(0); row++)
+            {
+                for (int col = 0; col < arr.GetLength(1); col++)
+                {
+                    if (arr[row, col] == 0)
+                    {
+                        x = row;
+                        y = col;
+                        return;
+                    }
+                }
+            }
+        }
+
+        private static void PrintMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write("{0,3}", matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
