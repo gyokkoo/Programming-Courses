abdf5c4e89c8de71a951099161d19a1dc5a07f83
Gyokan <gyokan.nezhdetov@gmail.com>
2016-02-16 21:33:31 +0200

    Array Lists Stacks Queues | Exercise
    Added first topic exercise.

---
+++ 1.Fundamental-Level/1.Advanced-C#/01.Arrays-Lists-Stacks-Queues/Exercise/1.BubbleSort/BubbleSort.cs
@@ -0,0 +1,33 @@
+﻿namespace _1.BubbleSort
+{
+    using System;
+    using System.Linq;
+
+    public class BubbleSort
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers.Length - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        SwapElementsInArray(j, j + 1, numbers);
+                    }
+                }
+            }
+
+            Console.WriteLine("[" + string.Join(", ", numbers) + "]");
+        }
+
+        private static void SwapElementsInArray(int i, int j, int[] numbers)
+        {
+            int exchangeValue = numbers[i];
+            numbers[i] = numbers[j];
+            numbers[j] = exchangeValue;
+        }
+    }
+}
---
+++ 1.Fundamental-Level/1.Advanced-C#/01.Arrays-Lists-Stacks-Queues/Exercise/2.JoinLists/JoinLists.cs
@@ -0,0 +1,31 @@
+﻿namespace _2.JoinLists
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class JoinLists
+    {
+        public static void Main()
+        {
+            SortedSet<int> joinedList = new SortedSet<int>();
+
+            List<int> firstList = new List<int>();
+            string[] firstInputLineArr = Console.ReadLine().Split(' ');
+            for (int i = 0; i < firstInputLineArr.Length; i++)
+            {
+                firstList.Add(int.Parse((firstInputLineArr[i])));
+                joinedList.Add(firstList[i]);
+            }
+
+            List<int> secondList = new List<int>();
+            string[] secondInputLineArr = Console.ReadLine().Split(' ');
+            for (int i = 0; i < secondInputLineArr.Length; i++)
+            {
+                secondList.Add(int.Parse((secondInputLineArr[i])));
+                joinedList.Add(secondList[i]);
+            }
+
+            Console.WriteLine(string.Join(", ", joinedList));
+        }
+    }
+}
