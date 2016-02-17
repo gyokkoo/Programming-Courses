ca5d35a54d90444f27f399c6c2b79e3082546aa4
Gyokan <gyokan.nezhdetov@gmail.com>
2016-02-17 23:16:34 +0200

    Methods | Exercise
    Added exercise on methods.

---
+++ 1.Fundamental-Level/1.Advanced-C#/03.Methods/Exercise/01.LetterCreator/LetterCreator.cs
@@ -0,0 +1,26 @@
+﻿namespace _01.LetterCreator
+{
+    using System;
+
+    public class LetterCreator
+    {
+        public static void Main()
+        {
+            string sender = Console.ReadLine();
+            string receiver = Console.ReadLine();
+
+            DateTime time = DateTime.Today;
+
+            PrintLetter(receiver, sender, time);
+        }
+
+        private static void PrintLetter(string sender, string receiver, DateTime time)
+        {
+            Console.WriteLine("Dear {0},", receiver);
+            Console.WriteLine("I hope I find you in good health.");
+            Console.WriteLine("I need to inform you that the cheese has run away.");
+            Console.WriteLine("Sincerely, {0}", sender);
+            Console.WriteLine(time.ToString("d"));
+        }
+    }
+}
---
+++ 1.Fundamental-Level/1.Advanced-C#/03.Methods/Exercise/02.UppercaseChars/UppercaseChars.cs
@@ -0,0 +1,31 @@
+﻿namespace _02.UppercaseChars
+{
+    using System;
+
+    public class UppercaseChars
+    {
+        public static void Main()
+        {
+            string str = Console.ReadLine();
+
+            Console.WriteLine(UppercaseCharsAtEvenPosition(str));
+        }
+
+        private static string UppercaseCharsAtEvenPosition(string str)
+        {
+            char[] arrayOfChars = str.ToCharArray();
+
+            for (int i = 0; i < arrayOfChars.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    arrayOfChars[i] = char.ToUpper(arrayOfChars[i]);
+                }
+            }
+
+            string uppercasedStr = new string(arrayOfChars);
+
+            return uppercasedStr;
+        }
+    }
+}
