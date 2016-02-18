dbfffaa63a613026b4e590000d75a9dde89cb526
Gyokan <gyokan.nezhdetov@gmail.com>
2016-02-18 16:26:06 +0200

    Strings and Text Processing | Exercise
    Added exercise on strings and text processing.

---
+++ 1.Fundamental-Level/1.Advanced-C#/04.Strings-and-Text-Processing/Exercise/1.CensorYourEmailAddress/CensorEmailAddress.cs
@@ -0,0 +1,24 @@
+﻿namespace _1.CensorYourEmailAddress
+{
+    using System;
+
+    public class CensorEmailAddress
+    {
+        public static void Main()
+        {
+            string correctEmail = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            int indexOfDomainStart = correctEmail.IndexOf('@');
+
+            string username = correctEmail.Substring(0, indexOfDomainStart);
+            string domain = correctEmail.Substring(indexOfDomainStart, (correctEmail.Length - username.Length));
+
+            string censoredEmail = new string('*', username.Length) + domain;
+
+            text = text.Replace(correctEmail, censoredEmail);
+
+            Console.WriteLine(text);
+        }   
+    }
+}
