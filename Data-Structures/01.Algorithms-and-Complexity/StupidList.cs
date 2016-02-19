3509b18f80f06da6167768f4b39df7c2a5c51ca6
Gyokan <gyokan.nezhdetov@gmail.com>
2016-02-19 15:32:40 +0200

    Added .cs file.

---
+++ Data-Structures/01.Algorithms-and-Complexity/StupidList.cs
@@ -0,0 +1,70 @@
+﻿namespace StupidClass
+{
+    using System;
+
+    public class StupidList<T>
+    {
+        private T[] arr = new T[0];
+        }
+
+        public int Length
+        {
+            get
+            {
+                return this.arr.Length;
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return this.arr[index];
+            }
+        }
+
+        public T First
+        {
+            get
+            {
+                return this.arr[0];
+            }
+        }
+
+        public T Last
+        {
+            get
+            {
+                return this.arr[this.arr.Length - 1];
+            }
+        }
+
+        public void Add(T item)
+        {
+            var newArr = new T[this.arr.Length + 1];
+            Array.Copy(this.arr, newArr, this.arr.Length);
+            newArr[newArr.Length - 1] = item;
+            this.arr = newArr;
+        }
+
+        public T Remove(int index)
+        {
+            T result = this.arr[index];
+            var newArr = new T[this.arr.Length - 1];
+            Array.Copy(this.arr, newArr, index);
+            Array.Copy(this.arr, index + 1, newArr, index, this.arr.Length - index - 1);
+            this.arr = newArr;
+            return result;
+        }
+
+        public T RemoveFirst()
+        {
+            return this.Remove(0);
+        }
+
+        public T RemoveLast()
+        {
+            return this.Remove(this.Length - 1);
+        }
+    }
+}
