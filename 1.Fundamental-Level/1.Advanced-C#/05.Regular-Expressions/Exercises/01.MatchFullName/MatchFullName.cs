1c678a0ad0e12706a1f656e28402f38958b2ad35
Gyokan <gyokan.nezhdetov@gmail.com>
2016-02-19 23:39:12 +0200

    Regular Expressions | Exercises
    Added regular expressions exercises.

---
+++ 1.Fundamental-Level/1.Advanced-C#/05.Regular-Expressions/Exercises/01.MatchFullName/MatchFullName.cs
@@ -0,0 +1,32 @@
+﻿namespace _01.MatchFullName
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+
+    public class MatchFullName
+    {
+        public static void Main()
+        {
+            const string Pettern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            List<string> names = new List<string>
+                                     {
+                                         "Ivan Ivanov",
+                                         "ivan ivanov",
+                                         "Ivan ivanov",
+                                         "ivan Ivanov",
+                                         "IVan Ivanov",
+                                         "Ivan IvAnov",
+                                         "Ivan  Ivanov"
+                                     };
+
+            Regex regex = new Regex(Pettern);
+            foreach (var name in names)
+            {
+                Console.Write("Is \"{0}\" correct name ? -> ", name);
+                Console.WriteLine(regex.IsMatch(name));
+            }
+        }
+    }
+}
---
+++ 1.Fundamental-Level/1.Advanced-C#/05.Regular-Expressions/Exercises/02.MatchPhoneNumber/MatchPhoneNumber.cs
@@ -0,0 +1,34 @@
+﻿namespace _02.MatchPhoneNumber
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+
+    public class MatchPhoneNumber
+    {
+        public static void Main()
+        {
+            const string Pattern = @"\+359\s2\s\d{3}\s\d{4}|\+359-2-\d{3}-\d{4}\b";
+
+            List<string> phoneNumbers = new List<string>
+                                            {
+                                                "+359 2 222 2222",
+                                                "+359-2-222-2222",
+                                                "359-2-222-2222",
+                                                "+359/2/222/2222",
+                                                "+359-2 222 2222",
+                                                "+359 2-222-2222",
+                                                "+359-2-222-222",
+                                                "+359-2-222-22222"
+                                            };
+
+            Regex regex = new Regex(Pattern);
+
+            foreach (var number in phoneNumbers)
+            {
+                Console.Write("Is \"{0}\" a valid phone number ? -> ", number);
+                Console.WriteLine(regex.IsMatch(number));
+            }
+        }
+    }
+}
