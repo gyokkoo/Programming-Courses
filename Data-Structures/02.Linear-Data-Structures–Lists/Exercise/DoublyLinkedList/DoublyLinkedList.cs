38cdd636a86242b0785ff590442224eebf8d403a
Gyokan <gyokan.nezhdetov@gmail.com>
2016-02-21 14:45:30 +0200

    List | Exercise
    Added exercise.

---
+++ Data-Structures/02.Linear-Data-Structures–Lists/Exercise/DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,147 @@
+﻿namespace Double_Linked_List
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class DoublyLinkedList<T> : IEnumerable<T>
+    {
+        private ListNode<T> head;
+        private ListNode<T> tail;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(T element)
+        {
+            var newNode = new ListNode<T>(element);
+            if (this.Count == 0)
+            {
+                this.head = newNode;
+                this.tail = newNode;
+            }
+            else
+            {
+                var newHead = newNode;
+                newHead.NextNode = this.head;
+                this.head.PreviousNode = newHead;
+                this.head = newHead;
+            }
+
+            this.Count++;
+        }
+
+        public void AddLast(T element)
+        {
+            if (this.Count == 0)
+            {
+                this.tail = this.head = new ListNode<T>(element);
+            }
+            else
+            {
+                var newTail = new ListNode<T>(element);
+                newTail.PreviousNode = this.tail;
+                this.tail.NextNode = newTail;
+                this.tail = newTail;
+            }
+
+            this.Count++;
+        }
+
+        public T RemoveFirst()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty.");
+            }
+
+            var firstElement = this.head.Value;
+            this.head = this.head.NextNode;
+            if (this.head != null)
+            {
+                this.head.PreviousNode = null;
+            }
+            else
+            {
+                this.tail = null;
+            }
+
+            this.Count--;
+            return firstElement;
+        }
+
+        public T RemoveLast()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty.");
+            }
+
+            var lastElement = this.tail.Value;
+            this.tail = this.tail.PreviousNode;
+            if (this.tail != null)
+            {
+                this.tail.NextNode = null;
+            }
+            else
+            {
+                this.head = null;
+            }
+
+            this.Count--;
+            return lastElement;
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            var currentNode = this.head;
+            while (currentNode != null)
+            {
+                action(currentNode.Value);
+                currentNode = currentNode.NextNode;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var currentNode = this.head;
+            while (currentNode != null)
+            {
+                yield return currentNode.Value;
+                currentNode = currentNode.NextNode;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public T[] ToArray()
+        {
+            var arr = new T[this.Count];
+            int index = 0;
+            var currentNode = this.head;
+            while (currentNode != null)
+            {
+                arr[index++] = currentNode.Value;
+                currentNode = currentNode.NextNode;
+            }
+
+            return arr;
+        }
+
+        private class ListNode<T>
+        {
+            public ListNode(T value)
+            {
+                this.Value = value;
+            }
+
+            public T Value { get; private set; }
+
+            public ListNode<T> NextNode { get; set; }
+
+            public ListNode<T> PreviousNode { get; set; }
+        }
+    }
+}
