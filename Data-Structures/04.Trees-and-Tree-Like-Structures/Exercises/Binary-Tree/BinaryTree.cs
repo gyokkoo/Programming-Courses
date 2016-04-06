7e43d91ea9ae69aa16a69c0315e3ea4c3c0b2b37
Gyokan <gyokan.nezhdetov@gmail.com>
2016-04-06 07:26:04 +0300

    Trees | Exercises
    Added trees lab.

---
+++ Data-Structures/04.Trees-and-Tree-Like-Structures/Exercises/Binary-Tree/BinaryTree.cs
@@ -0,0 +1,71 @@
+﻿namespace Trees
+{
+    using System;
+
+    public class BinaryTree<T>
+    {
+        public BinaryTree(
+            T value, 
+            BinaryTree<T> leftChild = null, 
+            BinaryTree<T> rightChild = null)
+        {
+            this.Value = value;
+            this.LeftChild = leftChild;
+            this.RightChild = rightChild;
+        }
+
+        public T Value { get; set; }
+
+        public BinaryTree<T> LeftChild { get; set; }
+
+        public BinaryTree<T> RightChild { get; set; }
+
+        public void PrintIndentedPreOrder(int indent = 0)
+        {
+            // Pre-order == root node, left child, right child
+            Console.Write(new string(' ', 2 * indent));
+            Console.WriteLine(this.Value);
+            if (this.LeftChild != null)
+            {
+                this.LeftChild.PrintIndentedPreOrder(indent + 1);
+            }
+
+            if (this.RightChild != null)
+            {
+                this.RightChild.PrintIndentedPreOrder(indent + 1);
+            }
+        }
+
+        public void EachInOrder(Action<T> action)
+        {
+            // In-order = left child, root node, right child
+            if (this.LeftChild != null)
+            {
+                this.LeftChild.EachInOrder(action);
+            }
+
+            action(this.Value);
+
+            if (this.RightChild != null)
+            {
+                this.RightChild.EachInOrder(action);
+            }
+        }
+
+        public void EachPostOrder(Action<T> action)
+        {
+            // Post-order == left child, right child, root node
+            if (this.LeftChild != null)
+            {
+                this.LeftChild.EachPostOrder(action);
+            }
+
+            if (this.RightChild != null)
+            {
+                this.RightChild.EachPostOrder(action);
+            }
+
+            action(this.Value);
+        }
+    }
+}
---
+++ Data-Structures/04.Trees-and-Tree-Like-Structures/Exercises/Binary-Tree/Tree.cs
@@ -0,0 +1,43 @@
+﻿namespace Trees
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Tree<T>
+    {
+        public Tree(T value, params Tree<T>[] children)
+        {
+            this.Value = value;
+            this.Children = new List<Tree<T>>();
+
+            foreach (var child in children)
+            {
+                this.Children.Add(child);
+            }
+        }
+
+        public T Value { get; set; }
+
+        public IList<Tree<T>> Children { get; private set; }
+
+        public void Print(int indent = 0)
+        {
+            Console.Write(new string(' ', 2 * indent));
+            Console.WriteLine(this.Value);
+            foreach (var child in this.Children)
+            {
+                child.Print(indent + 1);
+            }
+        }
+
+        public void Each(Action<T> action)
+        {
+            action(this.Value);
+
+            foreach (var child in this.Children)
+            {
+                child.Each(action);
+            }
+        }
+    }
+}
