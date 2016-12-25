a48445826849922b6d1d9c0f0c8624da1a7bb489
Gyokan <gyokan.nezhdetov@gmail.com>
2016-12-25 22:32:45 +0200

    Graphs-and-Graph-Algorithms | C# Lab

---
+++ Algorithms/05.Graphs-and-Graph-Algorithms/Lab-C#/DFS-Graph-Traversal/GraphConnectedComponents.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class GraphConnectedComponents
+{
+    private static List<int>[] graph;
+    private static bool[] visited;
+
+    public static void Main()
+    {
+        graph = ReadGraph();
+        FindGraphConnectedComponents();
+    }
+
+    private static List<int>[] ReadGraph()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var graph = new List<int>[n];
+        for (int i = 0; i < n; i++)
+        {
+            graph[i] = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+        }
+        return graph;
+    }
+
+    private static void FindGraphConnectedComponents()
+    {
+        visited = new bool[graph.Length];
+        for (int startNode = 0; startNode < graph.Length; startNode++)
+        {
+            if (!visited[startNode])
+            {
+                Console.Write("Connected component:");
+                DFS(startNode);
+                Console.WriteLine();
+            }
+        }
+    }
+
+    private static void DFS(int node)
+    {
+        if (!visited[node])
+        {
+            visited[node] = true;
+            foreach (var childNode in graph[node])
+            {
+                DFS(childNode);
+            }
+
+            Console.Write(" " + node);
+        }
+    }
+}
---
+++ Algorithms/05.Graphs-and-Graph-Algorithms/Lab-C#/Topological-Sorting/TopologicalSorter.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class TopologicalSorter
+{
+    private Dictionary<string, List<string>> graph;
+    private HashSet<string> visitedNodes;
+    private LinkedList<string> sortedNodes;
+    private HashSet<string> cycleNodes; 
+
+    public TopologicalSorter(Dictionary<string, List<string>> graph)
+    {
+        this.graph = graph;
+    }
+
+    //public ICollection<string> TopSort()
+    //{
+    //    var predecessorsCount = new Dictionary<string, int>();
+    //    foreach (var node in this.graph)
+    //    {
+    //        if (!predecessorsCount.ContainsKey(node.Key))
+    //        {   
+    //            predecessorsCount[node.Key] = 0;
+    //        }
+
+    //        foreach (var childNode in node.Value)
+    //        {
+    //            if (!predecessorsCount.ContainsKey(childNode))
+    //            {
+    //                predecessorsCount[childNode] = 0;
+    //            }
+
+    //            predecessorsCount[childNode]++;
+    //        }
+    //    }
+
+    //    var removedNodes = new List<string>();
+    //    while (true)
+    //    {
+    //        string nodeToRemove = this.graph.Keys.FirstOrDefault(n => predecessorsCount[n] == 0);
+    //        if (nodeToRemove == null)
+    //        {
+    //            break;
+    //        }
+
+    //        foreach (var node in this.graph[nodeToRemove])
+    //        {
+    //            predecessorsCount[node]--;
+    //        }
+
+    //        this.graph.Remove(nodeToRemove);
+    //        removedNodes.Add(nodeToRemove);
+    //    }
+
+    //    if (this.graph.Count > 0)
+    //    {
+    //        throw new InvalidOperationException("A cycle detected in the graph");
+    //    }
+
+    //    return removedNodes;
+    //}
+
+    public ICollection<string> TopSort()
+    {
+        this.visitedNodes = new HashSet<string>();
+        this.sortedNodes = new LinkedList<string>();
+        this.cycleNodes = new HashSet<string>();
+
+        foreach (var node in this.graph.Keys)
+        {
+            this.TopSortDFS(node);
+        }
+
+        return this.sortedNodes;
+    }
+
+    private void TopSortDFS(string node)
+    {
+        if (this.cycleNodes.Contains(node))
+        {
+            throw new InvalidOperationException("A cycle detected in the graph");
+        }
+
+        if (!this.visitedNodes.Contains(node))
+        {
+            this.visitedNodes.Add(node);
+            this.cycleNodes.Add(node);
+
+            if (this.graph.ContainsKey(node))
+            {
+                foreach (var childNode in this.graph[node])
+                {
+                    this.TopSortDFS(childNode);
+                }
+            }
+
+            this.cycleNodes.Remove(node);
+            this.sortedNodes.AddFirst(node);
+        }
+    }
+}
