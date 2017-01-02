a08ae70e1a10ff34d52113a6f8f5b3df68b54ab0
Gyokan <gyokan.nezhdetov@gmail.com>
2017-01-02 23:22:58 +0200

    Advanced Graph Algorithms | C# Lab

---
+++ Algorithms/06.Advanced-Graph-Algorithms/C#-Lab/Kurskal/KruskalAlgorithm.cs
@@ -0,0 +1,50 @@
+namespace Kurskal
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class KruskalAlgorithm
+    {
+        public static List<Edge> Kruskal(int numberOfVertices, List<Edge> edges)
+        {
+            edges.Sort();
+            var parent = new int[numberOfVertices];
+            for (int i = 0; i < numberOfVertices; i++)
+            {
+                parent[i] = i;
+            }
+
+            var spanningTree = new List<Edge>();
+            foreach (var edge in edges)
+            {
+                int rootStartNode = FindRoot(edge.StartNode, parent);
+                int rootEndNode = FindRoot(edge.EndNode, parent);
+                if (rootStartNode != rootEndNode)
+                {
+                    spanningTree.Add(edge);
+                    parent[rootStartNode] = rootEndNode;
+                }
+            }
+
+            return spanningTree;
+        }
+
+        public static int FindRoot(int node, int[] parent)
+        {
+            int root = node;
+            while (parent[root] != root)
+            {
+                root = parent[root];
+            }
+
+            while (node != root)
+            {
+                var oldParent = parent[node];
+                parent[node] = root;
+                node = oldParent;
+            }
+
+            return root;
+        }
+    }
+}
