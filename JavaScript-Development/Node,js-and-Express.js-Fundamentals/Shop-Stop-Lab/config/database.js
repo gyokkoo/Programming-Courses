20221705d0a455bba297e33e400c0aab6b5d2580
Gyokan <gyokan.nezhdetov@gmail.com>
2017-06-10 21:25:36 +0300

    Shop Stop using third-party modules
    Added next part of the lab
    minifying with gulp

--- JavaScript-Development/Node,js-and-Express.js-Fundamentals/Shop-Stop-Lab/config/database.js
+++ JavaScript-Development/Node,js-and-Express.js-Fundamentals/Shop-Stop-Lab/config/database.js
@@ -1,23 +1,33 @@
-let products = []
-let count = 1
+const fs = require('fs')
+const path = require('path')
+const dbPath = path.join(__dirname, '/database.json')
 
 module.exports.products = {}
 
-module.exports.products.getAll = () => {
+function getProducts () {
+  if (!fs.existsSync(dbPath)) {
+    fs.writeFileSync(dbPath, '[]')
+    return []
+  }
+  let json = fs.readFileSync(dbPath).toString() || '[]'
+  let products = JSON.parse(json)
   return products
 }
 
+function saveProducts (products) {
+  let json = JSON.stringify(products)
+  fs.writeFileSync(dbPath, json)
+}
+
+module.exports.products.getAll = getProducts
+
 module.exports.products.add = (product) => {
-  products.id = count++
+  let products = getProducts()
+  products.id = products.length + 1
   products.push(product)
+  saveProducts(products)
 }
 
 module.exports.products.findByName = (name) => {
-  let product = null
-  for (let p of products) {
-    if (name === p) {
-      return p
-    }
-  }
-  return product
+  return getProducts().filter(p => p.name.toLowerCase().includes(name))
 }
