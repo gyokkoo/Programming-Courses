611ca411ce697ce1fac3894f9acd5571c06087a8
Gyokan <gyokan.nezhdetov@gmail.com>
2018-01-13 23:40:45 +0200

    Added array list lab C#
    Simple array list implementation in C#

---
+++ Data-Structures/02.Linear-Data-Structures–Lists/ArrayList-Lab/Lists/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ArrayList<int> list = new ArrayList<int>();
+        int count = 10;
+
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine("Added element " + i);
+            list.Add(i);
+        }
+
+        Console.WriteLine("List count -> " + list.Count);
+
+        for (int i = 0; i < count; i++)
+        {
+            int removedElement = list.RemoveAt(0);
+            Console.WriteLine("Removed element " + removedElement);
+        }
+
+        Console.WriteLine("List count -> " + list.Count);
+    }
+}
